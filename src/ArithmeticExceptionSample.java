5157dea5a93c886f7f0478c0d32e3a8a7b8a9047
shiro-kyo <kyoka.shirasaka@linecorp.com>
2025-09-05 17:07:09 +0900

    javaについて

---
+++ src/ArithmeticExceptionSample.java
@@ -0,0 +1,9 @@
+
+public class ArithmeticExceptionSample {
+
+    public static void main(String[] args) {
+       int a = 10 / 0;
+       System.out.println(a);
+    }
+
+}
---
+++ src/Usevariable.java
@@ -0,0 +1,11 @@
+
+public class Usevariable {
+
+    public static void main(String[] args) {
+        int number2 = 100;
+        String str2 = "xyz";
+        System.out.print(number2);
+        System.out.print(str2);
+    }
+
+}
