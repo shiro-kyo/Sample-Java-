f1889f6b44aae6f1adbd7a0d33a610f386c2c70d
shiro-kyo <kyoka.shirasaka@linecorp.com>
2025-09-18 11:53:26 +0900

    Leeson6

---
+++ src/javaapis/string/StringComparison01.java
@@ -0,0 +1,15 @@
+package javaapis.string;
+
+public class StringComparison01 {
+
+    public static void main(String[] args) {
+        String str1 = new String("abc");
+        String str2 = new String("abc");
+
+        if (str1.equals(str2)) {
+            System.out.println("一致しました");
+        }else {
+            System.out.println("一致しませんでした！");
+        }
+    }
+}
---
+++ src/javaapis/string/SubstringSample.java
@@ -0,0 +1,10 @@
+package javaapis.string;
+
+public class SubstringSample {
+
+    public static void main(String[] args) {
+        String str = "あいうえお";
+        String sub = str.substring(3);
+        System.out.println(sub);
+    }
+}
